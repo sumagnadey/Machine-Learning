a255a0294547af2112f1b0d1070a84d25bf3629f
sumagnadey <sumagna.dey@gmail.com>
2019-08-22 23:25:21 +0530

    Add Label Encoder in KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -19,6 +19,10 @@ dataset.head()
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
+from sklearn.preprocessing import LabelEncoder
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.40)
 
