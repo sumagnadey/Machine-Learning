b4f654c3c0b6cc9cc7fbc91b6bcc9413ab38eb86
Aditya <adityashnkr5@gmail.com>
2019-08-22 22:20:02 +0530

    Add KNN code file

---
+++ KNN/KNN.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 21:59:55 2019
+
+@author: aditya
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+path = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+headernames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+dataset = pd.read_csv(path, names = headernames)
+dataset.head()
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.40)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 8)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+result = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(result)
+result1 = classification_report(y_test, y_pred)
+print("Classification Report:",)
+print (result1)
+result2 = accuracy_score(y_test,y_pred)
+print("Accuracy:",result2)
+
